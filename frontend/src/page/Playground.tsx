d8cd85cb53febf95d89c1aac438faee6c2f6b3a6
Zxilly <zhouxinyu1001@gmail.com>
2021-03-25 18:05:33 +0800

    feat: add markdown render

--- frontend/src/page/Playground.tsx
+++ frontend/src/page/Playground.tsx
@@ -1,14 +1,18 @@
 import infoList from "../list";
+import "./PlayGround.css";
+
 import { Redirect, useParams } from "react-router-dom";
-import { Box, Container, Grid, Paper } from "@material-ui/core";
+import { Box, Container, Grid, Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import { useEffect, useState } from "react";
+
 import AceEditor from "react-ace";
+import "ace-builds/webpack-resolver";
 import "ace-builds/src-noconflict/mode-c_cpp";
 import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/ext-language_tools";
 
-require("codemirror/mode/clike/clike");
+import Markdown from "markdown-to-jsx";
 
 interface PlaygroundRouteParams {
   section: string;
@@ -63,12 +67,18 @@ function Playground(): JSX.Element {
   const { section, content } = useParams<PlaygroundRouteParams>();
   const classes = useStyle();
   const [code, setCode] = useState("");
+  const [article, setArticle] = useState("");
 
   useEffect(() => {
     import(`!!raw-loader!../code/${section}/${content}.c`).then((code) => {
-      console.log(code.default);
+      // console.log(code.default);
       setCode(code.default);
     });
+    import(`!!raw-loader!../document/${section}/${content}.md`).then(
+      (article) => {
+        setArticle(article.default);
+      }
+    );
   }, [content, section]);
 
   if (!(section in infoList) || !(content in infoList[section].content)) {
@@ -79,15 +89,30 @@ function Playground(): JSX.Element {
       <Grid container spacing={2} className={classes.divideBox}>
         <Grid item xs={6} className={classes.divideBox}>
           <Box className={classes.cardBox0}>
-            <Paper className={classes.paper} elevation={0}>
-              test
+            <Paper
+              className={classes.paper}
+              elevation={2}
+              style={{ padding: "16px" }}
+            >
+              <Typography variant={"h2"} fontWeight={"bold"} fontSize={"24px"}>
+                {infoList[section].content[content].title}
+              </Typography>
+              <Markdown options={{ wrapper: "article" }}>{article}</Markdown>
             </Paper>
           </Box>
         </Grid>
         <Grid item xs={6} className={classes.divideBox}>
           <Box className={classes.cardBox1}>
-            <Paper className={classes.paper} elevation={0}>
+            <Paper
+              className={classes.paper}
+              elevation={2}
+              style={{ borderRadius: 0 }}
+            >
               <AceEditor
+                style={{
+                  width: "100%",
+                  height: "100%",
+                }}
                 mode="c_cpp"
                 theme="github"
                 name="editor"
@@ -95,7 +120,7 @@ function Playground(): JSX.Element {
                 showPrintMargin={true}
                 showGutter={true}
                 highlightActiveLine={true}
-                value={``}
+                value={code}
                 setOptions={{
                   enableBasicAutocompletion: true,
                   enableLiveAutocompletion: true,
@@ -107,7 +132,7 @@ function Playground(): JSX.Element {
             </Paper>
           </Box>
           <Box className={classes.cardBox2}>
-            <Paper className={classes.paper} elevation={0}>
+            <Paper className={classes.paper} elevation={2}>
               test
             </Paper>
           </Box>
