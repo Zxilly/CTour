fcefa9c425524167e6992be9a4d045e8cee8c01d
Zxilly <zhouxinyu1001@gmail.com>
2021-03-25 10:07:15 +0800

    feat: add ebox

--- frontend/src/page/Playground.tsx
+++ frontend/src/page/Playground.tsx
@@ -1,51 +1,46 @@
-import infoList from '../list'
-import {Redirect, useParams} from 'react-router-dom'
-import {Box, Card, Container} from "@material-ui/core";
-import {makeStyles} from "@material-ui/styles";
+import infoList from "../list";
+import { Redirect, useParams } from "react-router-dom";
+import { Box, Card, Container, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 interface PlaygroundRouteParams {
-    section: string,
-    content: string
+  section: string;
+  content: string;
 }
 
 const useStyle = makeStyles({
-    container: {
-        position: "absolute",
-        top: "64px",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        overflow: "auto"
-    },
-    divideBox: {
-        height: "100%",
-
-    }
-})
+  container: {
+    position: "absolute",
+    top: "64px",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    overflow: "auto",
+  },
+  divideBox: {
+    height: "100%",
+  },
+});
 
 function Playground(): JSX.Element {
-    const {section, content} = useParams<PlaygroundRouteParams>();
-    const classes = useStyle();
+  const { section, content } = useParams<PlaygroundRouteParams>();
+  const classes = useStyle();
 
-    if (!(section in infoList) || !(content in infoList[section].content)) {
-        return <Redirect to="/404"/>
-    }
-    return (
-        <Container className={classes.container}>
-            <Box>
-                <Card>
-
-                </Card>
-            </Box>
-            <Box>
-                <Card>
-
-                </Card>
-            </Box>
-
-
-        </Container>
-    )
+  if (!(section in infoList) || !(content in infoList[section].content)) {
+    return <Redirect to="/404" />;
+  }
+  return (
+    <Container className={classes.container}>
+      <Grid container>
+        <Grid item xs={6} className={}>
+          <Card />
+        </Grid>
+        <Grid item xs={6}>
+          <Card />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default Playground
+export default Playground;
