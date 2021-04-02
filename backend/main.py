9f080ddc903cbf9ffd66b714a97ff1ccab1627f8
Zxilly <zhouxinyu1001@gmail.com>
2021-04-02 19:22:24 +0800

    fix: add current env

--- backend/main.py
+++ backend/main.py
@@ -41,21 +41,26 @@ async def emcc_compile(
     #     ["emcc", "--sysroot", "/home/zxilly/wasi-sdk-12.0/share/wasi-sysroot", file_path, "-o", output_path],
     #     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    emcc = "/home/zxilly/emsdk/upstream/emscripten/emcc"
+    emcc = "/root/emsdk/upstream/emscripten/emcc"
+
+    env = os.environ.copy()
+    emccenv = {
+        "EMSDK": "/root/emsdk",
+        "EM_CONFIG": "/root/emsdk/.emscripten",
+        "EM_CACHE": "/root/emsdk/upstream/emscripten/cache",
+        "EMSDK_NODE": "/root/emsdk/node/14.15.5_64bit/bin/node",
+    }
 
     process = subprocess.Popen(
-        [emcc, file_path, "-o", output_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env={
-            "EMSDK": "/home/zxilly/emsdk",
-            "EM_CONFIG": "/home/zxilly/emsdk/.emscripten",
-            "EM_CACHE": "/home/zxilly/emsdk/upstream/emscripten/cache",
-            "EMSDK_NODE": "/home/zxilly/emsdk/node/14.15.5_64bit/bin/node",
-        }
+        [emcc, file_path, "-o", output_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=dict(env, **emccenv)
     )
 
     # emcc main.c -s FORCE_FILESYSTEM=1 -s EXPORT_ALL=1 -s STRICT=1 -s IGNORE_MISSING_MAIN=0 -s WASM_ASYNC_COMPILATION=0 -s MODULARIZE=1 -s EXPORT_ES6=1 -s EXIT_RUNTIME=1 -s ENVIRONMENT=web -o main.js
 
     try:
         returnCode = process.wait(timeout=15)
+        print(returnCode)
+
         if returnCode != 0:
             error = process.communicate()[1].decode()
         else:
@@ -67,6 +72,7 @@ async def emcc_compile(
         error = 'Compile Timeout'
 
     os.remove(file_path)
+    os.remove(output_path)
 
     return {"success": success, "wasm_id": random_name, "error": error}
 
@@ -84,7 +90,7 @@ async def emcc_compiled(
         with open(out, 'rb') as f:
             content = f.read()
 
-        # os.remove(out)
+        os.remove(out)
     except FileNotFoundError:
         raise HTTPException(404, 'Not Found')
 
@@ -92,4 +98,4 @@ async def emcc_compiled(
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0',debug=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=26546, debug=True)
