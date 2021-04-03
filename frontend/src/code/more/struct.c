372124989daa4ff99e8c2bcd11828e25acb40950
GCbzb <59912860+GCbzb@users.noreply.github.com>
2021-04-03 01:23:24 -0500

    feat: modify example data (#10)

--- frontend/src/code/more/struct.c
+++ frontend/src/code/more/struct.c
@@ -11,19 +11,19 @@ struct acb2
 {
 	char name[50];
 	int age;
-} s2 = {"李四", 22};
+} s2 = {"python", 1990};
 
 //省略结构体类型名
 struct
 {
 	char name[50];
 	int age;
-} s3 = {"孙悟空", 25};
+} s3 = {"java", 1995};
 
 int main(void)
 {
 	//先定义类型，再定义变量（常用）
-	struct abc s1 = {"张三", 18};
+	struct abc s1 = {"golang", 2009};
 
 	printf("%d,%s\n", s1.age, s1.name);
 	printf("%d,%s\n", s2.age, s2.name);
